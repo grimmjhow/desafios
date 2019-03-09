8cba8e579c11abe507df0b86fc800d0ba995b895
Felipe Monteiro <felipe.cife@gmail.com>
2019-03-09 19:54:13 -0300

    removendo imports não utilizados

--- crawlers/src/test/java/br/com/crawler/business/TelegramServiceTest.java
+++ crawlers/src/test/java/br/com/crawler/business/TelegramServiceTest.java
@@ -1,19 +1,10 @@
 package br.com.crawler.business;
 
-import java.util.List;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import com.pengrad.telegrambot.TelegramBot;
-import com.pengrad.telegrambot.model.Message;
-import com.pengrad.telegrambot.model.Update;
-import com.pengrad.telegrambot.request.GetUpdates;
-import com.pengrad.telegrambot.request.SendMessage;
-import com.pengrad.telegrambot.response.GetUpdatesResponse;
 
 import br.com.crawler.configuration.DependencyInjectionConfiguration;
 
