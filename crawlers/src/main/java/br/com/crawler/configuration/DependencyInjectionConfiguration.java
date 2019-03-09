13f3b7f9e8c38b61ebf82a4e5401063bdcf236b9
Felipe Monteiro <felipe.cife@gmail.com>
2019-03-09 19:43:00 -0300

    Retornando mapa de commands pelo Spring

--- crawlers/src/main/java/br/com/crawler/configuration/DependencyInjectionConfiguration.java
+++ crawlers/src/main/java/br/com/crawler/configuration/DependencyInjectionConfiguration.java
@@ -1,5 +1,8 @@
 package br.com.crawler.configuration;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,6 +10,9 @@ import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.request.GetUpdates;
 
 import br.com.crawler.business.MrCrawlerTelegramThread;
+import br.com.crawler.business.NadaPraFazerTelegramCommand;
+import br.com.crawler.business.StartTelegraCommand;
+import br.com.crawler.business.TelegramCommand;
 import br.com.crawler.model.CrawlerProvider;
 import br.com.crawler.model.JSoupCrawlerImpl;
 import br.com.crawler.services.RedditCrawlerService;
@@ -33,6 +39,16 @@ public class DependencyInjectionConfiguration {
 	public TelegramService telegramService(TelegramBot mrCrawler,RedditCrawlerService redditService) {
 		return new TelegramService(mrCrawler,redditService);
 	}*/
+	
+	@Bean
+	public Map<String, TelegramCommand> commandTelegram(TelegramBot mrCrawler,RedditCrawlerService redditService){
+
+		Map<String, TelegramCommand> command = new HashMap<>();
+		command.put("/nadaprafazer", new NadaPraFazerTelegramCommand(redditService,mrCrawler));
+		command.put("/start", new StartTelegraCommand(mrCrawler));
+		
+		return command;
+	}
 	
 	@Bean
 	public RedditCrawlerService redditService() {
